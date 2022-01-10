9f90093be3b02d72bc2df9f3ef4fba90ee0d88c1
SmokingPC <smokingpc@gmail.com>
2022-01-11 00:32:50 +0800

    add test2

---
+++ src/CudaTest2_BAR1Check/kernel.cu
@@ -0,0 +1,28 @@
+﻿
+#include "cuda.h"
+#include "nvml.h"
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+//#include "nv-p2p.h"
+
+#include <stdio.h>
+
+int main()
+{
+    cudaError_t status = cudaSetDeviceFlags(cudaDeviceMapHost);
+    status = cudaSetDevice(0);
+    CUDA_POINTER_ATTRIBUTE_P2P_TOKENS tokens;
+    CUdeviceptr ptr = 0;
+    CUresult result = CUDA_SUCCESS;
+    //cuDevicePrimaryCtxRetain();
+    result = cuMemAlloc(&ptr, 4096);
+    if(result == CUDA_SUCCESS)
+    {
+        result = cuPointerGetAttribute(&tokens, CU_POINTER_ATTRIBUTE_P2P_TOKENS, ptr);
+        cuMemFree(ptr);
+    }
+
+    cudaDeviceSynchronize();
+    cudaDeviceReset();
+    return 0;
+}
