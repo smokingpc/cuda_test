aae56f59f793dc2f23ebde063afcdcb11aab5dcc
SmokingPC <smokingpc@gmail.com>
2022-01-16 01:19:09 +0800

    update

--- src/CudaTest2_BAR1Check/kernel.cu
+++ src/CudaTest2_BAR1Check/kernel.cu
@@ -9,18 +9,27 @@
 
 int main()
 {
-    cudaError_t status = cudaSetDeviceFlags(cudaDeviceMapHost);
-    status = cudaSetDevice(0);
+    //cudaError_t status = cudaSetDeviceFlags(cudaDeviceMapHost);
+    cudaError_t status = cudaSetDevice(0);
     CUDA_POINTER_ATTRIBUTE_P2P_TOKENS tokens;
     CUdeviceptr ptr = 0;
     CUresult result = CUDA_SUCCESS;
     //cuDevicePrimaryCtxRetain();
     result = cuMemAlloc(&ptr, 4096);
+    cudaDeviceSynchronize();
     if(result == CUDA_SUCCESS)
     {
         result = cuPointerGetAttribute(&tokens, CU_POINTER_ATTRIBUTE_P2P_TOKENS, ptr);
+        if(result == CUDA_SUCCESS)
+            printf("p2pToken=%lld, vaSpaceToken=%d\n", tokens.p2pToken, tokens.vaSpaceToken);
+        else
+            printf("cuPointerGetAttribute failed (%d)\n", result);
+
+        cudaDeviceSynchronize();
         cuMemFree(ptr);
     }
+    else
+        printf("cuMemAlloc failed (%d)\n", result);
 
     cudaDeviceSynchronize();
     cudaDeviceReset();
